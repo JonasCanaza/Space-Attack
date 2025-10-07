1895740220a1fe3468847ca786730a6a55c404ce
Jonas <jonascanaza@gmail.com>
2025-10-07 17:51:18 -0300

    Final commit

--- conf.lua
+++ conf.lua
@@ -1,10 +1,10 @@
 require("src.globals")
 
 function love.conf(t)
-    t.console = true
+    t.console = false
 
     t.window.width = SCREEN_WIDTH
     t.window.height = SCREEN_HEIGHT
     t.window.title = "Space Attack"
-    t.window.icon = "res/icon.png"
+    t.window.icon = "res/game.png"
 end
